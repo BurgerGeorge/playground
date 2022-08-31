74ffdeca5a04652a31d89ca889fa06206cadeb7e
george <fgit2020@163.com>
2022-08-31 10:59:49 +0800

    ant commit[1]

--- src/main/java/org/young/GitTest.java
+++ src/main/java/org/young/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         }
         System.out.println("\n" + s);
         System.out.println("branch_cow did some change...");
+        System.out.println("[ant] added something...");
     }
 }
