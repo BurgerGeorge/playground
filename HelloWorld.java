1e632db05b83ba9a95f14cfb16a967eb303e6d0b
george <fgit2020@163.com>
2022-08-31 09:13:59 +0800

    commiting a java code

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+	String s = "Good Morning!";
+	for (int i = 0; i < s.length(); i++) {
+	    System.out.print("=");
+	}
+	System.out.println("\n" + s);
+    }
+}
+
